bd89a20faa0437e7c1e50654698b0768a22af259
MiniCraftt <nils@u1.se>
2021-04-20 23:02:46 +0200

    work

--- client/src/components/bubbleLinks/index.jsx
+++ client/src/components/bubbleLinks/index.jsx
@@ -4,7 +4,11 @@ const Boubbles = () => {
 
     return (
         <div className="bubbleContainer">
-            <div className="defaultBubble helpBubble fas fa-question" />
+            <div className="defaultBubble helpBubble fas fa-comments">
+                <div className="bubbleAlert">
+                    <div className="bubbleAlertCenter" />
+                </div>
+            </div>
             {/* <div className="defaultBubble twitterBubble fab fa-twitter" />
             <div className="defaultBubble instagramBubble fab fa-instagram" /> */}
         </div>
