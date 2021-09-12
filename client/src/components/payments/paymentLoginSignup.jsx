a3b1607f75d103d2d282a12674c9e5f7508c1cd7
ilmal <nils@u1.se>
2021-09-12 15:12:15 +0200

    work

--- client/src/components/payments/paymentLoginSignup.jsx
+++ client/src/components/payments/paymentLoginSignup.jsx
@@ -1,15 +1,32 @@
 // modules
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 // redux
 import { store } from "../../index"
-import { signup, login } from "../../redux/actions/index"
+import { signup, login, resendConfirmationMail } from "../../redux/actions/index"
 
 
 
 export default () => {
 
     const [showSignup, setShowSignup] = useState(true)
+    const [verifySignup, setVerifySignup] = useState(true)
+    const [clientNoMail, setClientNoMail] = useState(false)
+    const [user, setUser] = useState({ email: "loading" })
+
+    // updating store 
+    useEffect(() => {
+        if (store.getState().user != "This user doesn't exist") {
+            setUser(store.getState().user)
+        }
+    }, [])
+
+
+    // enable disable scroll paymentHandler
+    useEffect(() => {
+        if (verifySignup) return document.body.style.overflow = "hidden"
+        return document.body.style.overflow = "scroll"
+    }, [verifySignup])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -35,6 +52,10 @@ export default () => {
 
             // sending the name, email, pass and store to the redux action
             console.log("sending data")
+
+            // setting state for showing verify signup true
+            setVerifySignup(true)
+
             return signup(e.target.email.value.split("@")[0], e.target.email.value, e.target.pass1.value, store.dispatch)
         }
         // if the request is from the login page
@@ -57,10 +78,31 @@ export default () => {
                 </div>
             </form>
         )
-
     }
 
     const signupComponent = () => {
+        // showing signup verification if state verifySignup is true
+        if (verifySignup) return (
+            <div className="paymentVerificationSignupMainBody">
+                <div className="paymentVerificationSignupBody">
+                    <div className="paymentVerificationMessageBody">
+                        <span>Follow the link sent to <span> {user.email} </span> and verify you account!</span>
+                        <div className="noMailLine" />
+                        {clientNoMail ?
+                            <div className="noMail">
+                                <span>Make sure to check the spam folder!</span>
+                                <span>Still can't find it? Just send another one!</span>
+                                <button onClick={() => store.dispatch(resendConfirmationMail)}>Send another mail to {user.email}</button>
+                            </div>
+                            :
+                            <div>
+                                <span onClick={() => setClientNoMail(true)}>Can't find the email?</span>
+                            </div>
+                        }
+                    </div>
+                </div>
+            </div>
+        )
         return (
             <form className="paymentSignupMainContainer" onSubmit={handleSubmit}>
                 <span className="paymentLoginSignupHeader">SIGNUP</span>
@@ -70,7 +112,7 @@ export default () => {
                 <input name="pass2" className="paymentSignupPassword2" type="password" placeholder="Retype Password" required />
                 <div className="paymentLoginSignupButtonLinkContainer">
                     <button>Signup</button>
-                    <span onClick={() => setShowSignup(!showSignup)}>Don't have an account? Sign up</span>
+                    <span onClick={() => setShowSignup(!showSignup)}>Already have an account? Login</span>
                 </div>
             </form>
         )
