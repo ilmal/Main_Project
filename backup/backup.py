3c870b958cf449a8b0a06223e06ff6c4716be865
ilmal <nils@u1.se>
2021-12-21 23:17:13 +0100

    adding work to backup

--- backup/backup.py
+++ backup/backup.py
@@ -1,49 +1,24 @@
 import time
 import schedule
+import shutil
+import zipfile
+import os
+
+target_folder = "test_folder"
+output_name = "test_folder.zip"
 
 
-# import shutil
-# import zipfile
-# import os
-
-# target_folder = "test_folder"
-# output_name = "test_folder.zip"
-
-# if target_folder in os.listdir("."):
-#     shutil.rmtree(target_folder)
-
-# os.mkdir(target_folder)
-
-# for file in os.listdir("."):
-#     if file.startswith("."):
-#         print("File: " + file + "start with . (skipping)")
-#         continue
-#     if not file.startswith("test"):
-#         print("File: " + file + " don't start with test")
-#         continue
-#     if not file.endswith(".test"):
-#         print("File: " + file + " don't end with test")
-#         continue
-#     print("File: " + file + " is correct")
-#     shutil.copy("./" + file, target_folder)
-
-# if output_name in os.listdir("."):
-#     os.remove(output_name)
+def centralizeServerFiles(baseFolder):
+    print("bacseFolder: ", baseFolder)
+    print("hello")  # ADD SEARCH FOR "mc-servers" AND CENTRALIZE FILES
 
 
-# zip = zipfile.ZipFile(output_name, 'w', zipfile.ZIP_DEFLATED)
+def main(baseFolder):
+    if(baseFolder == None):
+        baseFolder = "/mnt/k8s-storage/kubedata"
 
-# shutil.make_archive(target_folder, 'zip', target_folder)
+    schedule.every(1).seconds.do(centralizeServerFiles, baseFolder)
 
-
-def main():
-    print("hello")
-
-
-schedule.every(1).seconds.do(main)
-schedule.every().hour.do(main)
-schedule.every().day.at("10:30").do(main)
-
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
--- backup/setup.py
+++ backup/setup.py
@@ -1,18 +1,29 @@
 import os
 import backup
+import sys
 
-files = os.listdir(".")
+path = None
 
 
-def reqFileFound():
+def reqFileFound(path):
     print("FILE FOUND: ")
     print("Installing packages")
     os.system("pip install -r requirements.txt")
     print("Package install complete, executing code!")
+    backup.main(path)
 
+
+# check if more than one argument is passed at startup
+if len(sys.argv) > 2:
+    print("More than one argument detected, please make sure to only add one variable at the startup")
+    sys.exit(1)
+if sys.argv[1].split("=")[0] == "PATH":
+    path = sys.argv[1].split("=")[1]
+
+
+files = os.listdir(".")
 
 for file in files:
     if file == "requirements.txt":
-        reqFileFound()
+        reqFileFound(path)
         continue
-    print("not found")
